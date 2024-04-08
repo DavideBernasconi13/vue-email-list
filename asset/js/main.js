95e1d71d83343114c17e6b414c629b4404611c08
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-08 17:46:21 +0200

    genera 10 elementi e gli stampa in pagina

--- asset/js/main.js
+++ asset/js/main.js
@@ -14,23 +14,26 @@ createApp({
             for (let i = 0; i < this.numberEmail; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
                     this.email = response.data.response;
-                    console.log('la mail è', this.email);
+                    //console.log('la mail è', this.email);
                     listaMail.push(this.email);
-                    if (i === this.numberEmail - 1) {
-                        this.emailList = [...listaMail]
+                    if (listaMail.length === this.numberEmail) {
+                        this.emailList = [...listaMail];
+                        //console.log('La lunghezza è', this.emailList.length)
+                        console.log(this.emailList);
                     }
-                    console.log(this.emailList);
+
+                    //console.log(this.emailList);
                 })
             }
-            console.log(listaMail);
+
 
         }
 
     },
     created() {
-        this.getEmailAddress();
+        // this.getEmailAddress();
     },
     mounted() {
-        console.log(this.email);
+        //console.log(this.email);
     }
 }).mount('#app'); 
