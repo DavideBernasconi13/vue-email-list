f1d5a4dc8d67c7852b2bf9f6edc1c80f06456674
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-08 16:05:23 +0200

    estrazione della mail e stampa a schermo

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,15 +3,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            test: 'test',
+            email: '',
+            numberEmail: 10
         }
     },
     methods: {
+        getEmailAddress() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                this.email = response.data.response;
+                console.log('la mail è', this.email);
+            })
+        }
 
     },
     created() {
+        this.getEmailAddress();
     },
     mounted() {
-        console.log(this.test)
+
+        console.log(this.email);
     }
 }).mount('#app'); 
